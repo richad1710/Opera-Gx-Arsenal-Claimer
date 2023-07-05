c09f29b62641ae1bf2edcbac3927e108e584d491
richad1710 <104232479+richad1710@users.noreply.github.com>
2023-07-05 16:21:36 -0300

    Posted

---
+++ operagxarsenal.py
@@ -0,0 +1,15 @@
+import requests
+
+heads = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+r = open('operagxarsenal.json', 'r')
+r2 = r.read()
+
+get_id = requests.post('https://users.roblox.com/v1/usernames/users', headers=heads, data=r2)
+print(get_id.json()['data'][0].get('id'))
+
+req2 = requests.post(f'https://www.opera.com/api/proxy/rolve/{get_id.json()["data"][0].get("id")}', headers=heads)
+print(req2.text)
